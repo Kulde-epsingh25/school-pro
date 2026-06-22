93dddb93b2160366ea80c1d59c51ba0ddf843e08
Kuldeep-singh25 <kuldeepsingh12311087@gmail.com>
2026-06-22 23:56:10 +0530

    basic structure update and react-ui import

---
+++ app/(frontend)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react';
+import SiteHeader from '@/components/site-header';
+export default function FrontLayout({children}: {
+  children: ReactNode;
+}) {
+  return (
+    <div>
+      <SiteHeader/>
+      {children}
+    </div>
+  );
+}
--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <h1>Welcome to the School Pro App</h1>
+    
    </div>
   );
 }
